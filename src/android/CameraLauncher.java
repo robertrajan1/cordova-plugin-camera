c18aebe7085d54b59e997cd3d4a16e7baa9f9821
robertrajan1 <107625896+robertrajan1@users.noreply.github.com>
2024-12-19 18:44:16 +0530

    Update CameraLauncher.java

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -227,18 +227,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
 
         if (android.os.Build.VERSION.SDK_INT >= 33) {
             // Android API 33 and higher
-              switch (mediaType) {
-               case PICTURE:
-                   permissions.add("android.permission.READ_MEDIA_IMAGES");
-                   break;
-               case VIDEO:
-                   permissions.add("android.permission.READ_MEDIA_VIDEO");
-                   break;
-               default:
-                   permissions.add("android.permission.READ_MEDIA_IMAGES");
-                   permissions.add("android.permission.READ_MEDIA_VIDEO");
-                   break;
-           }
+            permissions.add(Manifest.permission.CAMERA);
         } else {
             // Android API 32 or lower
             permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
